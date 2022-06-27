ec0a7b211fcbbcaf102dafad063994e809813bdf
Airei Fukuzawa <eyeray@fb.com>
2022-06-27 09:33:50 -0700

    Initial Commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by Airei Fukuzawa on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
