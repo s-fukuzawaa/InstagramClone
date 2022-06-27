4ba5ff2fe4c6f07ef368d6756887974ad586efe4
Airei Fukuzawa <eyeray@fb.com>
2022-06-27 13:15:05 -0700

    assets added

---
+++ InstagramClone/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  InstagramClone
+//
+//  Created by Airei Fukuzawa on 6/27/22.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *pwField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)loginButton:(id)sender {
+}
+- (IBAction)registerButton:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
